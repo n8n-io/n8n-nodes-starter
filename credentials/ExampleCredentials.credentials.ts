7ac69f816eb5dd43c53b7317f864ddb92b1401b9
Omar Ajoue <krynble@gmail.com>
2022-06-29 17:25:14 +0200

    Improve example credential

--- credentials/ExampleCredentials.credentials.ts
+++ credentials/ExampleCredentials.credentials.ts
@@ -1,4 +1,4 @@
-import { ICredentialType, NodePropertyTypes } from 'n8n-workflow';
+import { IAuthenticateGeneric, ICredentialTestRequest, ICredentialType, NodePropertyTypes } from 'n8n-workflow';
 
 export class ExampleCredentials implements ICredentialType {
 	name = 'exampleCredentials';
@@ -8,16 +8,44 @@ export class ExampleCredentials implements ICredentialType {
 		// Properties can be defined exactly in the same way
 		// as node properties.
 		{
-			displayName: 'User',
-			name: 'user',
+			displayName: 'User Name',
+			name: 'username',
 			type: 'string' as NodePropertyTypes,
 			default: '',
 		},
 		{
-			displayName: 'Access Token',
-			name: 'accessToken',
+			displayName: 'Password',
+			name: 'password',
 			type: 'string' as NodePropertyTypes,
+			typeOptions: {
+				password: true,
+			},
 			default: '',
 		},
 	];
+
+	// This credential is currently not used by any node directly
+	// but the HTTP Request node can use it to make requests.
+	// The credential is also testable due to the `test` property below
+	authenticate = {
+		type: 'generic',
+		properties: {
+			auth: {
+				username: '={{ $credentials.username }}',
+				password: '={{ $credentials.password }}',
+			},
+			qs: {
+				// Send this as part of the query string
+				n8n: 'rocks',
+			}
+		},
+	} as IAuthenticateGeneric;
+
+	// The block below tells how this credential can be tested
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://example.com/',
+			url: '',
+		},
+	};
 }
