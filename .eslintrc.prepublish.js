994a677b3e06f64eac2cf71f670fea43e8eb9e9f
Iván Ovejero <ivov.src@gmail.com>
2022-07-19 13:03:24 +0200

    :shirt: Enable `community-package-json-name-still-default` on publish

---
+++ .eslintrc.prepublish.js
@@ -0,0 +1,12 @@
+module.exports = {
+	extends: "./.eslintrc.js",
+	overrides: [
+		{
+			files: ['package.json'],
+			plugins: ['eslint-plugin-n8n-nodes-base'],
+			rules: {
+				'n8n-nodes-base/community-package-json-name-still-default': 'error',
+			},
+		},
+	],
+};
