bd868dcc2bff66c52ae13d830e42fe1e6631a48f
MrMatiz2 <alejandromatiz87@gmail.com>
2025-07-10 17:07:51 -0500

    icon not being copied solved

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,6 @@
-const path = require('path');
 const { task, src, dest } = require('gulp');
 
-task('build:icons', copyIcons);
-
-function copyIcons() {
-	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
-	const nodeDestination = path.resolve('dist', 'nodes');
-
-	src(nodeSource).pipe(dest(nodeDestination));
-
-	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
-	const credDestination = path.resolve('dist', 'credentials');
-
-	return src(credSource).pipe(dest(credDestination));
-}
+task('build:icons', function () {
+  return src(['nodes/**/*.png', 'nodes/**/*.svg'], { base: 'nodes' })
+    .pipe(dest('dist/nodes'));
+});
